22531b3b708b38cce4d3d36e31e3fb53dc8bd6c0
Djothi Grondin <djothi.grondin@live.fr>
2017-02-12 15:15:26 +0400

    landing pages

--- client/main.js
+++ client/main.js
@@ -2,3 +2,13 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 import '../imports/startup/accounts-config.js';
+
+
+Template.intro.onRendered(function () {
+  this.$('.parallax').parallax();
+});
+
+
+Template.menu.onRendered(function () {
+  this.$(".button-collapse").sideNav();
+});
